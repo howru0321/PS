f0cc55c1314b37a61301984554ed68d65544dfe8
howru0321 <118611763+howru0321@users.noreply.github.com>
2023-06-21 15:50:33 +0900

    Create p1.cpp

---
+++ Array/p1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+
+int func(int a[], int n)
+{
+	int num[101] = { 0 };
+	for (int i = 0; i < n; i++)
+	{
+		num[a[i]]++;
+	}
+
+	for (int i = 0; i < 50; i++)
+	{
+		if (num[i] && num[100 - i])
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	int arr[1000];
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << func(arr, N);
+	
+
+	return 0;
+}
