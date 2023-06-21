f02dee703f97dd43c0aa1a37c3213cf5d74b2cc9
howru0321 <118611763+howru0321@users.noreply.github.com>
2023-06-21 15:22:18 +0900

    Create p0.cpp

---
+++ Array/p0.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string word;
+int ans[26];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    
+	getline(cin, word);
+
+	for (char c : word)
+	{
+		ans[c - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++) 
+	{
+		cout << ans[i] << " ";
+	}
+
+
+	return 0;
+}
